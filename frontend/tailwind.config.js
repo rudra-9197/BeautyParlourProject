5baa8b5308b8d3429bf03800ae6db953c9f781c9
rudra-9197 <91685979+rudra-9197@users.noreply.github.com>
2024-04-28 23:59:15 +0530

    intial commit

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    colors:{
+      navpink: '#ec4899',
+      white: '#FFFFFF',
+      grayc: '#bbb',
+      grayb:"#717171",
+      black: '#000000'
+    },
+ 
+    extend: {},
+  },
+  plugins: [],
+}
